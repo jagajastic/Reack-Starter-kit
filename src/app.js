f6ad69b081366e951299a6760579b98fe56b996a
ibrahim <josephibrahi@gmail.com>
2019-03-27 10:18:52 +0100

    Setup(Simple app): added a simple app for the starter kit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Hello world</h1>
+                <p>My first react app for the year 2019</p>
+            </div>
+        )
+    }
+}
+
+export default App;
